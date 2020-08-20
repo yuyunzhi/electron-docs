5634fe07fc4fad80229967b73643782e44036fb3
yuyunzhi <lambert1129@foxmail.com>
2020-08-20 15:45:47 +0800

    feat：init 本地调试

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const isDev = require('electron-is-dev')
+
+app.on('ready', () => {
+  let mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  console.log('isDev', isDev);
+  
+  const urlLocation = isDev ? 'http://localhost:3000' : 'dummyUrl'
+  mainWindow.loadURL(urlLocation)
+  mainWindow.webContents.openDevTools()
+  // 监听渲染进程发送的事件
+  ipcMain.on('message', (event, arg) => {
+    console.log(event)
+    console.log(arg)
+    event.reply('reply', 'hello from main process')
+  })
+  // let secondWindow = new BrowserWindow({
+  //   width: 400,
+  //   height: 300,
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   },
+  //   parent: mainWindow
+  // })
+  // secondWindow.loadFile('second.html')
+})
