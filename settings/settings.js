d8a8a3bb483c68e3b140272996ad3a9635d646cd
yuyunzhi <lambert1129@foxmail.com>
2020-08-27 19:24:42 +0800

    bugfix

--- settings/settings.js
+++ settings/settings.js
@@ -1,7 +1,12 @@
 const { remote, ipcRenderer } = require('electron')
 const Store = require('electron-store')
-const settingsStore = new Store({name: 'Settings'})
-const qiniuConfigArr = ['#savedFileLocation','#accessKey', '#secretKey', '#bucketName']
+const settingsStore = new Store({ name: 'Settings' })
+const qiniuConfigArr = [
+  '#savedFileLocation',
+  '#accessKey',
+  '#secretKey',
+  '#bucketName'
+]
 
 const $ = (selector) => {
   const result = document.querySelectorAll(selector)
@@ -14,25 +19,27 @@ document.addEventListener('DOMContentLoaded', () => {
     $('#savedFileLocation').value = savedLocation
   }
   // get the saved config data and fill in the inputs
-  qiniuConfigArr.forEach(selector => {
+  qiniuConfigArr.forEach((selector) => {
     const savedValue = settingsStore.get(selector.substr(1))
     if (savedValue) {
       $(selector).value = savedValue
     }
   })
   $('#select-new-location').addEventListener('click', () => {
-    remote.dialog.showOpenDialog({
-      properties: ['openDirectory'],
-      message: '选择文件的存储路径',
-    }, (path) => {
-      if (Array.isArray(path)) {
-        $('#savedFileLocation').value = path[0]
-      }
-    })
+    remote.dialog
+      .showOpenDialog({
+        properties: ['openDirectory'],
+        message: '选择文件的存储路径'
+      })
+      .then(({ filePaths: path }) => {
+        if (Array.isArray(path)) {
+          $('#savedFileLocation').value = path[0]
+        }
+      })
   })
   $('#settings-form').addEventListener('submit', (e) => {
     e.preventDefault()
-    qiniuConfigArr.forEach(selector => {
+    qiniuConfigArr.forEach((selector) => {
       if ($(selector)) {
         let { id, value } = $(selector)
         settingsStore.set(id, value ? value : '')
@@ -44,11 +51,11 @@ document.addEventListener('DOMContentLoaded', () => {
   })
   $('.nav-tabs').addEventListener('click', (e) => {
     e.preventDefault()
-    $('.nav-link').forEach(element => {
+    $('.nav-link').forEach((element) => {
       element.classList.remove('active')
     })
     e.target.classList.add('active')
-    $('.config-area').forEach(element => {
+    $('.config-area').forEach((element) => {
       element.style.display = 'none'
     })
     $(e.target.dataset.tab).style.display = 'block'
