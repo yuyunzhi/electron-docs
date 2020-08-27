c136ae30eda0fe95d2de8c16be17440b267f43d6
yuyunzhi <lambert1129@foxmail.com>
2020-08-27 21:18:08 +0800

    feat: 上传和下载

--- test.js
+++ test.js
@@ -18,30 +18,27 @@ const config = new qiniu.conf.Config();
 config.zone = qiniu.zone.Zone_z0;
 
 // 上传
-// const localFile = "/Users/yuyunzhi/Documents/5.md";
-// const formUploader = new qiniu.form_up.FormUploader(config);
-// const putExtra = new qiniu.form_up.PutExtra();
-// const key='5.md';
+const localFile = "/Users/yuyunzhi/Documents/5.md";
+const formUploader = new qiniu.form_up.FormUploader(config);
+const putExtra = new qiniu.form_up.PutExtra();
+const key='5.md';
 // 文件上传
-// formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
-//   respBody, respInfo) {
-//   if (respErr) {
-//     throw respErr;
-//   }
-//   console.log('statusCode', respInfo.statusCode);
+formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
+  respBody, respInfo) {
+  if (respErr) {
+    throw respErr;
+  }
+  console.log('statusCode', respInfo.statusCode);
   
-//   if (respInfo.statusCode == 200) {
-//     console.log('respBody', respBody);
-//   } else {
-//     console.log(respInfo.statusCode);
-//     console.log(respBody);
-//   }
-// });
-
-
+  if (respInfo.statusCode == 200) {
+    console.log('respBody', respBody);
+  } else {
+    console.log(respInfo.statusCode);
+    console.log(respBody);
+  }
+});
 
 // 下载
-
 const bucketManager = new qiniu.rs.BucketManager(mac, config);
 const publicBucketDomain = 'http://qfq3hdf02.hd-bkt.clouddn.com';
 const key='3.jpg';
