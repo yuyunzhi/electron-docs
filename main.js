fca00f8cdc1ac1806478b618ab035f5c9ea3c391
yuyunzhi <lambert1129@foxmail.com>
2020-08-27 18:27:25 +0800

    feat:添加设置窗口功能和逻辑

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
+const path = require('path')
+const AppWindow = require('./src/AppWindow')
 const isDev = require('electron-is-dev')
 const menuTemplate = require('./src/menuTemplate')
 
 app.on('ready', () => {
-  let mainWindow = new BrowserWindow({
-    width: 1424,
-    height: 680,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  console.log('isDev', isDev)
 
-  const urlLocation = isDev ? 'http://localhost:3000' : 'dummyUrl'
-  mainWindow.loadURL(urlLocation)
+  const mainWindowConfig = {
+    width: 1440,
+    height: 768,   
+  }
+  
+  const urlLocation = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './index.html')}`
+  mainWindow = new AppWindow(mainWindowConfig, urlLocation)
   mainWindow.webContents.openDevTools()
 
   let menu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(menu)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+  // 创建设置窗口
+  ipcMain.on('open-settings-window', () => {
+
+    const settingsWindowConfig = {
+      width: 500,
+      height: 400,
+      parent: mainWindow  
+    }
+
+    const settingsFileLocation = `file://${path.join(__dirname, './settings/settings.html')}`
+    // 创建设置窗口 
+    settingsWindow = new AppWindow(settingsWindowConfig, settingsFileLocation)
+    settingsWindow.removeMenu()
+    settingsWindow.on('closed', () => {
+      settingsWindow = null
+    })
+  })
 
   // 监听渲染进程发送的事件
   ipcMain.on('message', (event, arg) => {
