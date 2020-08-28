4dedaa0d423964a66c06b28241c0ccb241ed5717
yuyunzhi <lambert1129@foxmail.com>
2020-08-28 14:24:08 +0800

    feat:动态修改菜单

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const path = require('path')
 const AppWindow = require('./src/AppWindow')
 const isDev = require('electron-is-dev')
+const Store = require('electron-store')
 const menuTemplate = require('./src/menuTemplate')
-
+const QiniuManager = require('./src/utils/QiniuManager')
+const settingsStore = new Store({ name: 'Settings'})
+const fileStore = new Store({name: 'Files Data'})
 app.on('ready', () => {
 
   const mainWindowConfig = {
@@ -38,12 +41,32 @@ app.on('ready', () => {
     settingsWindow.on('closed', () => {
       settingsWindow = null
     })
+
+  })
+
+
+  ipcMain.on('config-is-saved', () => {
+    // watch out menu items index for mac and windows
+    let qiniuMenu = process.platform === 'darwin' ? menu.items[3] : menu.items[2]
+    console.log('qiniuMenu.submenu.items',qiniuMenu.submenu.items);
+    
+    const switchItems = (toggle) => {
+      [1, 2, 3].forEach(number => {
+        qiniuMenu.submenu.items[number].enabled = toggle
+      })
+    }
+    const qiniuIsConfiged =  ['accessKey', 'secretKey', 'bucketName'].every(key => !!settingsStore.get(key))
+    if (qiniuIsConfiged) {
+      switchItems(true)
+    } else {
+      switchItems(false)
+    }
   })
 
   // 监听渲染进程发送的事件
-  ipcMain.on('message', (event, arg) => {
-    console.log(event)
-    console.log(arg)
-    event.reply('reply', 'hello from main process')
-  })
+  // ipcMain.on('message', (event, arg) => {
+  //   console.log(event)
+  //   console.log(arg)
+  //   event.reply('reply', 'hello from main process')
+  // })
 })
